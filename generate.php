84599fb01f8d31123dfd4ce70867d4c31bfbe5a0
jsx <saurabh@rebugged.com>
2013-11-19 07:45:24 +0530

    Removed the lines used to check if the script is running locally

--- generate.php
+++ generate.php
@@ -2,11 +2,7 @@
 
 require_once("dompdf/dompdf_config.inc.php");
 
-// We check wether the user is accessing the demo locally
-$local = array("::1", "127.0.0.1");
-$is_local = in_array($_SERVER['REMOTE_ADDR'], $local);
-
-if ( isset( $_POST["html"] ) && $is_local ) {
+if ( isset( $_POST["html"] )) {
 
   if ( get_magic_quotes_gpc() )
     $_POST["html"] = stripslashes($_POST["html"]);
