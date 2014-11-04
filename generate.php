15d6febf2489e2c0ace7412f416a9ba749c082b4
tanay1337 <tanay1337@gmail.com>
2014-11-04 15:14:32 +0530

    Implemented the functionality to generate back side of the card

--- generate.php
+++ generate.php
@@ -2,14 +2,16 @@
 
 require_once("dompdf/dompdf_config.inc.php");
 
-if ( isset( $_POST["html"] )) {
+if ( isset( $_POST["html"] ) && isset( $_POST["html2"] )) {
 
   if (get_magic_quotes_gpc()) {
       $_POST["html"] = stripslashes($_POST["html"]);
+      $_POST["html2"] = stripslashes($_POST["html2"]);
   }
-  
+
+  $card_data = $_POST["html"].$_POST["html2"];
   $dompdf = new DOMPDF();
-  $dompdf->load_html($_POST["html"]);
+  $dompdf->load_html($card_data);
   $dompdf->set_paper($_POST["paper"], $_POST["orientation"]);
   $dompdf->render();
 
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
         <title>Card Generator | Mozilla Portal | Mozilla Kerala</title>
         <meta name="description" content="Card Generator for Mozillians">
-
+  
         <!--    favicon     -->
         <link rel="shortcut icon" type="image/ico" href="images/favicon.ico">
 
@@ -34,15 +34,17 @@
             $(document).ready(function () {
                 $('#gen-card-button').click(function () {
                     var template_html = $("#gen-template-frame").contents().find('html').html();
+                    var template_back_html = $("#gen-template-back-frame").contents().find('html').html();
                     $('#gen-html-textarea').val(template_html);
+                    $('#gen-html-back-textarea').val(template_back_html);
                     $('#gen-create-form').submit();
                 });
             });
         </script>
     </head>
-
+    
     <body>
-
+        
         <div id="page">
             <a href="/en-US/" id="tabzilla">mozilla</a>
 
@@ -61,27 +63,31 @@
             </div>
 
             <p class="help-text"><span class="heart">❤</span> Press <span class="shortcut-key">SHIFT</span> to highlight editable text. And click to edit.</p>
-
+            
             <!-- TODO: Need to change this to support choosing other card templates. -->
             <iframe id="gen-template-frame" src="templates/Reps-Card 1.html"></iframe>
-
+            <iframe id="gen-template-back-frame" src="templates/back/Reps-Card 1.html"></iframe>
+            
             <form id="gen-create-form" action="generate.php" method="post">
                 <input name="paper" type="hidden" value="card"/>
                 <input name="orientation" type="hidden" value="portrait"/>
 
                 <textarea name="html" id="gen-html-textarea"></textarea>
+                <textarea name="html2" id="gen-html-back-textarea"></textarea>
                 <script>
-                  	function change() {
+                  	function change() { 
                   		var sel = $('#gen-select-template option:selected').text();
                   		var frame = $('#gen-template-frame');
+                  		var frame_back = $('#gen-template-back-frame');
   						frame.attr('src', 'templates/'+sel+'.html');
+  						frame_back.attr('src', 'templates/back/'+sel+'.html');
   					}
                 </script>
                 <div>
                     <input type="button" value="Generate Card" id="gen-card-button" class="button" />
                 </div>
             </form>
-
+            
             <div id="footer"><br/>Created by fox lovers in Kerala | <a href="https://github.com/MozillaKerala/Card-Generator/">Grab the code</a> and improve it. | <a href="https://github.com/MozillaKerala/Card-Generator/tree/master#how-to-add-new-card-template">Add</a> a new card template.<br/><br/></div>
 
         </div>
@@ -98,7 +104,6 @@
             });
         </script>
 
-	<!--	Google Analytics	-->
         <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -107,27 +112,8 @@
 
           ga('create', 'UA-46056779-4', 'mozillakerala.org');
           ga('send', 'pageview');
+
         </script>
-
-	<!--	Piwik Analytics	-->
-	<script type="text/javascript">
-		var _paq = _paq || [];
-		_paq.push(['trackPageView']);
-		_paq.push(['enableLinkTracking']);
-		(function() {
-			var u="//piwik.mozillakerala.org/";
-			_paq.push(['setTrackerUrl', u+'piwik.php']);
-			_paq.push(['setSiteId', 6]);
-			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-			g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-		})();
-	</script>
-
-	<noscript>
-		<p><img src="//piwik.mozillakerala.org/piwik.php?idsite=6" style="border:0;" alt="" /></p>
-	</noscript>
-	<!-- End Piwik Code -->
-
     </body>
 
 </html>
