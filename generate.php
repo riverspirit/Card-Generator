766d116b9606439c1fff409f704668d69fb51bec
Gabriel Mičko <gabrielmicko@Gabriels-MacBook-Pro.local>
2016-01-17 23:12:11 +0100

    Resolving images absolute path dependency

--- generate.php
+++ generate.php
@@ -1,9 +1,16 @@
 <?php
 
 require_once("vendor/autoload.php");
+require_once("config.php");
 
 use Dompdf\Dompdf;
 use Dompdf\Options;
+
+function resolveDependency($html) {
+	$html = str_replace('../fonts/', 'fonts/', $html);
+	$html = str_replace('src="../', 'src="'. $GLOBALS['IMG_PATH'], $html);
+	return $html;
+}
 
 if ( isset( $_POST[ "html" ] ) && isset( $_POST[ "html2" ] ) ) {
 
@@ -12,8 +19,7 @@ if ( isset( $_POST[ "html" ] ) && isset( $_POST[ "html2" ] ) ) {
 		$_POST[ "html2" ] = stripslashes( $_POST[ "html2" ] );
 	}
 
-	$card_data = $_POST[ "html" ] . $_POST[ "html2" ];
-	$card_data = str_replace('../fonts/', 'fonts/', $card_data);
+	$card_data = resolveDependency($_POST[ "html" ] . $_POST[ "html2" ]);
 	$dompdf = new Dompdf();
 	$options = new Options();
 	$options->set( 'isRemoteEnabled', true );
