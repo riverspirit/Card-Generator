840927921cfec37a64b0bddfbb9dad2da24693df
Gabriel Mičko <micsko@gmail.com>
2016-05-21 19:17:32 +0200

    fix #37

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
 
 //If you are running this from subdir it comes handy xy.com/SubDir/...
-$GLOBALS['SUBDIR'] = '/Card-Generator';
+$GLOBALS['SUBDIR'] = '';
 //Fixing dependency issues
 $GLOBALS['RESOURCE'] = $protocol . $_SERVER['HTTP_HOST'] . $GLOBALS['SUBDIR'] . '/';
 
