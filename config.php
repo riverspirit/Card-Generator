2c94a979570e7a0546ff0301b8127525d7610107
Gabriel Mičko <micsko@gmail.com>
2016-05-21 17:31:48 +0200

    Footer fix

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
 
 //If you are running this from subdir it comes handy xy.com/SubDir/...
-$GLOBALS['SUBDIR'] = '';
+$GLOBALS['SUBDIR'] = '/Card-Generator';
 //Fixing dependency issues
 $GLOBALS['RESOURCE'] = $protocol . $_SERVER['HTTP_HOST'] . $GLOBALS['SUBDIR'] . '/';
 
