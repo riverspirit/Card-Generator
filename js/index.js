48468b4f7928afaf52388a17245fd8ca3dba0ac8
Gabriel Mičko <micsko@gmail.com>
2016-05-23 22:15:45 +0200

    Avatar support

--- js/index.js
+++ js/index.js
@@ -1,12 +1,34 @@
 $(document).on('keydown', function (e) {
   if (e.keyCode === 16) {
     $('#gen-template-frame').contents().find('[contenteditable]').css('background-color', 'rgba(255, 100, 58, 0.43)');
+    $('.upload-avatar').addClass('show');
   }
 });
 
 $(document).on('keyup', function (e) {
   $('#gen-template-frame').contents().find('[contenteditable]').css('background-color', 'transparent');
+  $('.upload-avatar').removeClass('show');
 });
+
+
+   function previewFile(){
+       var preview = document.querySelector('img'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+         console.log(reader.result);
+           //preview.src = reader.result;
+           $('#gen-template-frame').contents().find('.logo img').attr('src', reader.result);
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+  }
+
 
 function change() {
   var sel = $('#gen-select-template option:selected').text();
